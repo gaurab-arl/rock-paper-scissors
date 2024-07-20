d781c64faf9137a18df5a06fcb4fee5af05c580d
Gaurab <159932313+gaurab-arl@users.noreply.github.com>
2024-07-20 18:27:21 +0545

    Update rock paper siccor.c

--- rock paper siccor.c
+++ rock paper siccor.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <windows.h>
+
+// Define color constants
+#define COLOR_DEFAULT 7   // White
+#define COLOR_RED     12  // Red
+#define COLOR_GREEN   10  // Green
+#define COLOR_BLUE    9   // Blue
 
 // Function prototypes
+void setcolor(int color);
 void printMenu();
 char getComputerChoice();
 char getUserChoice();
@@ -10,7 +18,7 @@ void determineWinner(char userChoice, char computerChoice);
 
 int main() {
     char userChoice, computerChoice;
-    srand(time(0));                                                                             // Seed random number generator
+    srand(time(0));  // Seed random number generator
 
     while (1) {
         printMenu();
@@ -23,6 +31,11 @@ int main() {
         printf("Computer chose: %c\n", computerChoice);
 
         determineWinner(userChoice, computerChoice);
+
+        // Pause briefly before clearing screen to allow user to see result
+        printf("\nPress Enter to continue...");
+        getchar();  // Consume newline left in buffer by scanf
+        getchar();  // Wait for Enter key press
     }
 
     printf("Thanks for playing!\n");
@@ -30,17 +43,20 @@ int main() {
 }
 
 void printMenu() {
+    system("cls");
     printf("\nRock-Paper-Scissors Game\n");
     printf("Choose one of the following options:\n");
     printf("R - Rock\n");
     printf("P - Paper\n");
     printf("S - Scissors\n");
     printf("Q - Quit\n");
+    setcolor(COLOR_BLUE);
     printf("Enter your choice: ");
+    setcolor(COLOR_DEFAULT);
 }
 
 char getComputerChoice() {
-    int randomChoice = rand() % 3;                                                                   // 
+    int randomChoice = rand() % 3;
     switch (randomChoice) {
         case 0:
             return 'R';
@@ -60,14 +76,26 @@ char getUserChoice() {
 }
 
 void determineWinner(char userChoice, char computerChoice) {
+    // Convert choices to uppercase for case-insensitive comparison
+    userChoice = toupper(userChoice);
+    computerChoice = toupper(computerChoice);
+
     if (userChoice == computerChoice) {
+        setcolor(COLOR_GREEN);
         printf("It's a tie!\n");
     } else if ((userChoice == 'R' && computerChoice == 'S') ||
                (userChoice == 'P' && computerChoice == 'R') ||
                (userChoice == 'S' && computerChoice == 'P')) {
+        setcolor(COLOR_GREEN);
         printf("You win!\n");
     } else {
+        setcolor(COLOR_RED);
         printf("You lose!\n");
     }
+    setcolor(COLOR_DEFAULT);
 }
 
+void setcolor(int color) {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, color);
+}
